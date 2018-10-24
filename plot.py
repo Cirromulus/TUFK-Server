529cf2cb596b0976cde7a8be5cebd76ae19180fd
Pascal Pieper <accounts@pascalpieper.de>
2018-10-24 12:39:07 +0200

    better hour locator

--- plot.py
+++ plot.py
@@ -12,8 +12,9 @@ r=db.store_result()
 data = r.fetch_row(0)
 
 timestamp = [datetime.datetime.utcfromtimestamp(int(time)) for (time, _ , _ , _) in data]
-temp = [float(temp)for (_ ,temp , _ , _) in data]
-humid = [float(humid)for (_ , _ , humid , _) in data]
+temp = [float(temp) for (_ ,temp , _ , _) in data]
+humid = [float(humid) for (_ , _ , humid , _) in data]
+#heater = [time for (time, _, _, status ) in data]
 
 fig = plt.figure()
 axT = fig.add_subplot(111)
@@ -26,7 +27,7 @@ axH.plot(timestamp, humid, label="Humidity", color='b')
 axH.set_ylim(0, 100)
 plt.ylabel("Humidity in rel. %")
 
-axT.xaxis.set_minor_locator(dates.HourLocator(interval=5))   # every 4 hours
+axT.xaxis.set_minor_locator(dates.HourLocator(byhour=[x*6 for x in range(1,int(24/6))]))
 axT.xaxis.set_minor_formatter(dates.DateFormatter('%H:%M'))  # hours and minutes
 axT.xaxis.set_major_locator(dates.DayLocator(interval=1))    # every day
 axT.xaxis.set_major_formatter(dates.DateFormatter('\n%d.%m.%Y')) 
