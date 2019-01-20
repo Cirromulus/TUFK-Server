93aeae11dec77aa71a9759fda3b6955f8db35da4
Pascal Pieper <accounts@pascalpieper.de>
2019-01-20 13:47:05 +0100

    Now cam remote

---
+++ cam.php
@@ -0,0 +1,21 @@
+<html>
+<head></head>
+<body>
+<?php
+$output = array();
+if(isset($_POST['update']))
+{
+	echo "Update: ".$_POST['update'];
+	$res = exec("ssh -v -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 './takePicture.sh' 2>&1", $output, $return);
+}
+?>
+<img src="/image.jpg" style="max-width: 100%; max-height:80%; margin:auto; display: block;" alt="Dasn Bild."/>
+<form method="post"><button name="update" value="Ja">Neu Aufnehmen</button></form><br />
+<?php
+foreach($output as $line)
+{
+	echo $line."<br />";
+}
+?>
+</body>
+</html>
