8183a721d11e2a4372f869b1bde90cbe078eb276
Pascal Pieper <accounts@pascalpieper.de>
2019-01-29 15:39:48 +0100

    Moar automate

--- config.php
+++ config.php
@@ -1,8 +1,12 @@
 <?php
 include_once("settings.php");
-
 echo '<?xml version="1.0" encoding="UTF-8"?>
-<Config targetTemperature="'.$config["targetTemperature"].'" temp_lower_limit="'.$config["temp_lower_limit"].'" temp_upper_limit="'.$config["temp_upper_limit"].'" humid_lower_limit="'.$config["humid_lower_limit"].'" humid_upper_limit="'.$config["humid_upper_limit"].'" targetHumidity="'.$config["targetHumidity"].'" samplingPeriodSeconds="'.$config["samplingPeriodSeconds"].'" serverConnectionPeriodSeconds="'.$config["serverConnectionPeriodSeconds"].'" serverURI="http://wiewarmistesbei.exsilencio.de/"/>';
+<Config ';
+foreach($confignames as $name)
+{
+	echo $name[0].'="'.$config[$name[0]].'" ';
+}
+echo 'serverURI="http://wiewarmistesbei.exsilencio.de/"/>';
 
 $conn->close();
 ?>
