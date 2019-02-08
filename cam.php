6ca63f283b1302f6c2dd62291782f68197ffe004
Pascal Pieper <accounts@pascalpieper.de>
2019-02-08 17:58:31 +0100

    Now correct file name

--- cam.php
+++ cam.php
@@ -9,7 +9,7 @@ if(isset($_POST['update']))
 	$res = exec("ssh -v -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 './takePicture.sh' 2>&1", $output, $return);
 }
 ?>
-<img src="/image.jpg?<?php echo filemtime('image.png');?>" style="max-width: 100%; max-height:80%; margin:auto; display: block;" alt="Dasn Bild."/>
+<img src="/image.jpg?<?php echo filemtime('image.jpg');?>" style="max-width: 100%; max-height:80%; margin:auto; display: block;" alt="Dasn Bild."/>
 <form method="post"><button name="update" value="Ja">Neu Aufnehmen</button></form><br />
 <?php
 foreach($output as $line)
