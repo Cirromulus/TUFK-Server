c4997697248a36ebe3186230eaa18aeb908a7576
Pascal Pieper <accounts@pascalpieper.de>
2019-02-10 12:40:15 +0100

    Removed verbose logging

--- cam.php
+++ cam.php
@@ -6,7 +6,7 @@ $output = array();
 if(isset($_POST['update']))
 {
 	echo "Update: ".$_POST['update'];
-	$res = exec("ssh -v -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 './takePicture.sh' 2>&1", $output, $return);
+	$res = exec("ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 './takePicture.sh' 2>&1", $output, $return);
 }
 ?>
 <img src="/image.jpg?<?php echo filemtime('image.jpg');?>" style="max-width: 100%; max-height:80%; margin:auto; display: block;" alt="Dasn Bild."/>
