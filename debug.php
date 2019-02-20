dc62da65d5d38340d8b60f76470f5812a193a685
Pascal Pieper <accounts@pascalpieper.de>
2019-02-20 14:34:27 +0100

    Added restart of service to debug pafe

--- debug.php
+++ debug.php
@@ -16,6 +16,11 @@ if(isset($_POST['update']))
                 $res = exec("ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 irsend SEND_ONCE HEATER ONOFF ; sleep 0.25 ; irsend SEND_ONCE HEATER UP", $output, $return);
 		echo "command sent.";
         }
+	else if($_POST['update'] == "restart")
+        {
+                $res = exec("ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i id_rsa pi@10.8.0.5 sudo systemctl restart controller", $output, $return);
+		echo "command sent.";
+        }
 	else
 	{
 		echo "unsupported";
@@ -24,10 +29,17 @@ if(isset($_POST['update']))
 else
 {
 ?>
-	<form method="post"><button name="update" value="onoff">ONOFF Senden</button></form><br />
-	Note: Can only switch heater off if on, if heater is off, it will get in an interesting different state :o<br />
-	<form method="post"><button name="update" value="onoffPlus">ONOFF mit UP Senden</button></form><br />
-	Note: Can only switch heater on if off, if heater is on, it will get in an interesting different state :o<br />
+	<form method="post">
+		Note: Can only switch heater off if on, if heater is off, it will get in an interesting different state :o<br />
+		<button name="update" value="onoff">ONOFF Senden</button>
+	</form><br />
+	<form method="post">
+		Note: Can only switch heater on if off, if heater is on, it will get in an interesting different state :o<br />
+		<button name="update" value="onoffPlus">ONOFF mit UP Senden</button>
+	</form><br />
+	<form method="post">
+		<button name="update" value="restart">Restart Service</button>
+	</form><br />
 <?php
 }
 foreach($output as $line)
